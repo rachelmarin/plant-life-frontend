0d378e9bdfa79e5a636eec1bb1a90656f5fe9381
rachelmarin <Rachelmarinpal@gmail.com>
2021-04-30 11:42:47 -0400

    constructor home

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    
+    return (
+       <div>
+         <h1> Welcome to the Plant Fam </h1>
+         <p>this is a little extra</p>
+       </div>
+    )
+}
+
+
+export default Home;
