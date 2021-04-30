c8da68904ea16f5780a09259d18045ca2b4e2c8b
rachelmarin <Rachelmarinpal@gmail.com>
2021-04-30 11:40:32 -0400

    app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import  { connect } from 'react-redux';
+import { fetchPlants} from './actions/Index';
+import PlantForm from './components/PlantForm';
+import PlantList from './components/PlantList';
+import Plant from './components/Plant';
+import Home from './components/Home';
+import Nav from './components/Nav';
 
-function App() {
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchPlants();
+  }
+
+  render() {
+    if ( this.props.loading ) {
+      return <div>Loading....</div>
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+          <Nav/>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/plants/new" component={ PlantForm}/> 
+              <Route exact path="/plants" render={props => <PlantList  { ...props } plants={ this.props.plants } /> }/>
+              <Route exact path="/plants/:id" render={props => <Plant { ...props } plants={ this.props.plants } /> } />
+            </Switch>
+      </Router>
   );
 }
+}
 
-export default App;
+
+const mapStateToProps = state => {
+  return {
+    plants: state.plants,
+    loading: state.loading
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+   fetchPlants: () => dispatch(fetchPlants()) 
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
