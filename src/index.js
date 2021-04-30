03af901b800af4097292cf6bda1dbe830abcbf57
rachelmarin <Rachelmarinpal@gmail.com>
2021-04-30 13:01:14 -0400

    applied middleware

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
+import {applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 
 import PlantsReducer from'./reducers/PlantsReducer';
 
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore( PlantsReducer )
+const store = createStore( PlantsReducer, applyMiddleware( thunk) )
 ReactDOM.render(
   <React.StrictMode>
   <Provider store={store} >
