25a66c937833ccb70652fec9e0e300a27eb1798a
rachelmarin <Rachelmarinpal@gmail.com>
2021-05-03 19:59:43 -0400

    started myplants

---
+++ src/components/MyPlants.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class MyPlants extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>My Plants</h2>
+            </div>
+        )
+    }
+}
+
+export default MyPlants;
