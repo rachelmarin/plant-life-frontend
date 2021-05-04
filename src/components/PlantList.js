9716a4c496525f14fe980b4c5fab653af2b77631
rachelmarin <Rachelmarinpal@gmail.com>
2021-05-03 20:01:55 -0400

    fixed title

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,26 +1,28 @@
 import React, {Component} from 'react';
 import PlantCard from './PlantCard';
 
+
 class PlantList extends Component {
 
+    
 
 render() {
     const plants = this.props.plants.map( (plant, index) => (
         <PlantCard
          key={ index }
-         name={ plant.name }
+         title={ plant.title }
          id={ plant.id}
         />
     ))
     return (
-        <div>
-            <h3>Plant List</h3>
-           { plants }
+        <div className='plant-listing'>
+            <h2>Plant List</h2>
+            
+            {plants}
+           
         </div>
-    )
-}
-
-
+        )
+    }
 
 }
 
